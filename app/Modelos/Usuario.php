cd850af9e0ee29d557d958f9dcae5938a4f7a096
kiikedls <jespinozadelossatnos@gmail.com>
2019-04-09 14:56:32 -0500

    desactive los timestamps en la base de datos

--- app/Modelos/Usuario.php
+++ app/Modelos/Usuario.php
@@ -7,6 +7,8 @@ use App\Modelos\Persona;
 
 class Usuario extends Model
 {
+    public $timestamps=false;
+
     protected $table = 'usuario';
     protected $fillable = ['NomUsuario', 'PassUsuario', 'CodEmp', 'api_token'];
 

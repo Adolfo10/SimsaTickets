c2354aac761b96f54b46d3c196cada2c7e647d59
kiikedls <jespinozadelossatnos@gmail.com>
2019-04-09 15:12:30 -0500

    ahora la pagina web verifica contraseñas encriptadas y cambie los ifelse de la seleccion de tipo de usuario por un switch

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Modelos\Persona;
 use App\Modelos\TipoPersona;
 use App\Modelos\Usuario;
+use Illuminate\Support\Facades\Hash;
 use Session;
 
 class HomeController extends Controller
@@ -21,30 +22,33 @@ class HomeController extends Controller
       $GetUs  = $request->input("NomUsuario");
       $GetPas = $request->input("PassUsuario");
       
-      $usuario = Usuario::where('NomUsuario', '=', $GetUs)->where('PassUsuario', '=', $GetPas)->get()->first();
-      
+//      $usuario = Usuario::where('NomUsuario', '=', $GetUs)->where('PassUsuario', '=', $GetPas)->get()->first();
+      $usuario = Usuario::where('NomUsuario', '=', $GetUs)->get()->first();
+
       if($usuario != null){
-        $persona = Persona::find($usuario->CodEmp);
-        
-        Session::put('persona', $persona);
-        if($persona->CodTipoPersona == 1) 
-            {             
-                Session::put('img', 'root');
-                Session::put('tipoPer','Administrador'); 
-                return redirect('/root');
-            }
-        else if($persona->CodTipoPersona == 2)
-            {                
-                Session::put('img', 'tec');
-                Session::put('tipoPer','Técnico'); 
-                return redirect('/tec');
-            }
-        else if($persona->CodTipoPersona == 3)
-            {            
-                Session::put('img', 'emp');
-                Session::put('tipoPer','Empleado'); 
-              return redirect('/emp');
-            }
+          $pass=$usuario->PassUsuario;
+          if (Hash::check($GetPas,$pass)) {
+              $persona = Persona::find($usuario->CodEmp);
+              Session::put('persona', $persona);
+
+              switch ($persona->CodTipoPersona){
+                  case 1:
+                      Session::put('img', 'root');
+                      Session::put('tipoPer', 'Administrador');
+                      return redirect('/root');
+                      break;
+                  case 2:
+                      Session::put('img', 'tec');
+                      Session::put('tipoPer', 'Técnico');
+                      return redirect('/tec');
+                      break;
+                  case 3:
+                      Session::put('img', 'emp');
+                      Session::put('tipoPer', 'Empleado');
+                      return redirect('/emp');
+                      break;
+              }
+          }
       }
       
         return redirect('/')->with("Error","Usuario y/o contraseña incorrectos");

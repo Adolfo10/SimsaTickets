f73bb91390194af8ac71d9d96da8ff80262c1e75
kiikedls <jespinozadelossatnos@gmail.com>
2019-04-10 12:14:09 -0500

    sexto intento by kiike

--- app/Http/Controllers/AndroidController.php
+++ app/Http/Controllers/AndroidController.php
@@ -20,40 +20,18 @@ class AndroidController extends Controller
     function IniciarSesion(Request $request){
         $GetUs  = $request->input("NomUsuario");
         $GetPas = $request->input("PassUsuario");
+        $passenc=Hash::make($GetPas);
         $datos=[];
 
-        $usuario = Usuario::where('NomUsuario', '=', $GetUs)->get()->first();
+        $usuario = Usuario::where('NomUsuario', '=', $GetUs)->where('PassUsuario','=',$passenc)->get()->first();
 
         if($usuario != null){
-            $pass=$usuario->PassUsuario;
-            if (Hash::check($GetPas,$pass)) {
                 $persona = Persona::find($usuario->CodEmp);
-//                Session::put('persona', $persona);
                 $datos['user']=$usuario;
                 $datos['persona']=$persona;
-
-                switch ($persona->CodTipoPersona){
-                    case 1:
-                       // Session::put('img', 'root'); kiko
-                       // Session::put('tipoPer', 'Administrador');
-//                        return redirect('/root');
-                    return $datos;
-                        break;
-                    case 2:
-                        Session::put('img', 'tec');
-                        Session::put('tipoPer', 'Técnico');
-                        return redirect('/tec');
-                        break;
-                    case 3:
-                        Session::put('img', 'emp');
-                        Session::put('tipoPer', 'Empleado');
-                        return redirect('/emp');
-                        break;
-                }
-            }
         }
 
-        return redirect('/')->with("Error","Usuario y/o contraseña incorrectos");
+        return $datos;
     }
 
 }

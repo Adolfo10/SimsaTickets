6ab38a24c02453477babae67a9719bee62a64c66
aplicaciones3 <juankno4@gmail.com>
2019-04-11 10:59:50 -0500

    Pruebas Rutas

--- app/Http/Controllers/AndroidEmpController.php
+++ app/Http/Controllers/AndroidEmpController.php
@@ -15,13 +15,13 @@ class AndroidEmpController extends Controller
 
 //         $id = $request->input("id");
          $datoshist = [];
-         // me quede haciendo lo del historial del empleado asi que mañana lo ago del comparar keys para saber cual empleado es
 
          $historial= DB::table('problema')
              ->join('seguimiento', 'problema.id', '=', 'seguimiento.problema')
              ->join('tipoproblema', 'problema.CodTipoProblema', '=', 'tipoproblema.id')
              ->join('equipotrabajo', 'problema.CodEqTrab', '=', 'equipotrabajo.id')
              ->join('personas', 'equipotrabajo.CodEmp', '=', 'personas.id')
+             ->join('problema_persona', 'personas.id', '=', 'problema_persona.CodEmp')
              ->where('personas.id','=', '21')
              ->select('seguimiento.fecha_prob', 'seguimiento.hora_prob', 'problema.id',
                  'equipotrabajo.Descripcion', 'tipoproblema.NombreProblema',
@@ -29,8 +29,8 @@ class AndroidEmpController extends Controller
              ->orderBy('seguimiento.fecha_prob', 'desc')
              ->get();
 
-
          $datoshist["historial"] = $historial;
+
          return ($datoshist);
      }
 }

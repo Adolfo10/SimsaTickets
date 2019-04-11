4040f74f920ece20cfc116055c4cd4e1aff79108
aplicaciones3 <juankno4@gmail.com>
2019-04-11 18:36:15 -0500

    Pruebas Rutas AndroidEmp

--- app/Http/Controllers/EmpController.php
+++ app/Http/Controllers/EmpController.php
@@ -35,12 +35,15 @@ class EmpController extends Controller
         $Equipos = EquipoTrabajo::whereHas('persona')->with('persona')
             ->where('CodEmp', '=', Session::get('persona')->id)->get();
 
+
+        dd($Equipos);
+
         $historial= DB::table('problema')
             ->join('seguimiento', 'problema.id', '=', 'seguimiento.problema')
             ->join('tipoproblema', 'problema.CodTipoProblema', '=', 'tipoproblema.id')
             ->join('equipotrabajo', 'problema.CodEqTrab', '=', 'equipotrabajo.id')
             ->join('personas', 'equipotrabajo.CodEmp', '=', 'personas.id')
-            ->where('problema.CodEqTrab', "=", $Equipos[5]->CodEmp)
+            ->where('problema.CodEqTrab', "=", $Equipos[0]->CodEmp)
            ->select('seguimiento.fecha_prob', 'seguimiento.hora_prob', 'problema.id',
                     'equipotrabajo.Descripcion', 'tipoproblema.NombreProblema',
                     'problema.prioridad', 'problema.estatus')

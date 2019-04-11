d007f67f1b709ad54203803523e38baa1ad0233a
aplicaciones3 <juankno4@gmail.com>
2019-04-11 11:02:03 -0500

    Pruebas Rutas

--- app/Http/Controllers/AndroidEmpController.php
+++ app/Http/Controllers/AndroidEmpController.php
@@ -21,8 +21,6 @@ class AndroidEmpController extends Controller
              ->join('tipoproblema', 'problema.CodTipoProblema', '=', 'tipoproblema.id')
              ->join('equipotrabajo', 'problema.CodEqTrab', '=', 'equipotrabajo.id')
              ->join('personas', 'equipotrabajo.CodEmp', '=', 'personas.id')
-             ->join('problema_persona', 'personas.id', '=', 'problema_persona.CodEmp')
-             ->where('problema_persona.CodEmp','=', '3')
              ->select('seguimiento.fecha_prob', 'seguimiento.hora_prob', 'problema.id',
                  'equipotrabajo.Descripcion', 'tipoproblema.NombreProblema',
                  'problema.prioridad', 'problema.estatus')

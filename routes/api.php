20b5a3cc5b23dd0a931f3c7a56d1f0a284322fd4
kiikedls <jespinozadelossatnos@gmail.com>
2019-04-19 15:58:11 -0500

    cree una ruta para recuperar a todos los empleados para asignarles un equipo de trabajo

--- routes/api.php
+++ routes/api.php
@@ -98,5 +98,5 @@ Route::post('/mostrarHist',function (){
 
 Route::post('/mostrarEmp',function (){
     $empleados=new AndroidRootController();
-    return ["personas"=>$empleados->recuperarEmpleados()];
+    return [$empleados->recuperarEmpleados()];
 });

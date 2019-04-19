315bd20f14cb9e70103919444deb182e171bb736
gerardoadame <gera_skate@outlook.com>
2019-04-19 00:27:57 -0500

    nuevo

--- app/Http/Controllers/EmpController.php
+++ app/Http/Controllers/EmpController.php
@@ -33,8 +33,8 @@ class EmpController extends Controller
     function Historial(){
 
 
-        // $Equipos = EquipoTrabajo::whereHas('persona')->with('persona')
-        //     ->where('CodEmp', '=', Session::get('persona')->id)->get();
+        $Equipos = EquipoTrabajo::whereHas('persona')->with('persona')
+            ->where('CodEmp', '=', Session::get('persona')->id)->get();
 
         $historial= DB::table('problema')
             ->join('seguimiento', 'problema.id', '=', 'seguimiento.problema')

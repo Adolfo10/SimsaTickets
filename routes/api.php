ecc2d332ac2762ce7a15ae3ec5925a90faecb3f3
kiikedls <jespinozadelossatnos@gmail.com>
2019-04-20 18:37:42 -0500

    devolver un arreglo con todos los problemas de un empleado

--- routes/api.php
+++ routes/api.php
@@ -69,7 +69,7 @@ Route::post('/actualizar', function (Request $r){
 
 Route::post('/history',function (Request $r){
     $his = new AndroidEmpController();
-    return $his->history($r);
+    return $his->history($r->get('id'));
 });
 
 

b9c65a464b7fdab4460bdf082f7c587b8adcf9e8
gerardoadame <gera_skate@outlook.com>
2019-04-20 18:56:29 -0500

    cambios en el api y AndroidTecController

--- app/Http/Controllers/AndroidTecController.php
+++ app/Http/Controllers/AndroidTecController.php
@@ -17,7 +17,7 @@ class AndroidTecController extends Controller
     {
 
         $dato=[];
-        $tecnico=Persona::find($request->input("idTec"));
+        $tecnico=Persona::find($request->input("id"));
         $tecnico->NomEmp=$request->input('NomEmp');
         $tecnico->ApPat=$request->input('ApPat');
         $tecnico->ApMat=$request->input('ApMat');
@@ -25,8 +25,7 @@ class AndroidTecController extends Controller
         $tecnico->CelEmp=$request->input('CelEmp');
         $tecnico->EmailEmp=$request->input('EmailEmp');
         $tecnico->save();
-        $dato["Persona"] = $tecnico;
 
-        return $dato;
+        return ;
     }
 }

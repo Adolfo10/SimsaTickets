285bbdca58bea6388f120ee77ad5c26fcd084156
gerardoadame <gera_skate@outlook.com>
2019-04-25 16:47:10 -0500

    cambios en el tecnico

--- app/Http/Controllers/AndroidTecController.php
+++ app/Http/Controllers/AndroidTecController.php
@@ -48,6 +48,6 @@ class AndroidTecController extends Controller
         //         ->select('problema.id','personas.NomEmp','tipoproblema.NombreProblema')
         //         ->get();
         
-        return $coleccion->first()->get("histec");
+        return $coleccion->first()->get();
     }
 }

6ebc2d0f49e2d51f26e3d79a72a5354302f82bfd
kiikedls <jespinozadelossatnos@gmail.com>
2019-04-29 08:13:25 -0500

    provando devolver datos de los problemas al historial root

--- app/Http/Controllers/AndroidRootController.php
+++ app/Http/Controllers/AndroidRootController.php
@@ -9,6 +9,7 @@ use App\Modelos\Problema;
 use App\Modelos\TipoProblema;
 use App\Modelos\Usuario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
